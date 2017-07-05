1a9fdb4bfd8076c8ce886b26f65d0f0631c24e84
MAHANANDA NYAMAGOUDA <mnanda021@users.noreply.github.com>
2017-07-05 14:02:52 -0500

    Add files via upload

---
+++ winestore.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Fashion Club-Club</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/layout.css" type="text/css">
+<link href="css/dropdown.css" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+	function start(){
+		var current = new Date();
+		document.getElementById("strings").innerHTML="<p style=\"color:red;text-align:right;font-size:15px;\">Date : "+current.toString()+"</p>";
+			}
+	window.addEventListener("load",start,false);
+	</script>
+</head>
+<body>
+<div class="wrapper row1">
+  <header id="header" class="clear">
+    <div id="hgroup">
+      <h1><a href="home.html">Fashion Club</a></h1>
+      <h2>The joy of dressing is an Art!</h2>
+    </div>
+    <nav>
+     <ul>
+      <ul class="dropdown">
+        <li><a href="Club.html">Club</a>
+        <div class="dropdown-content">
+    <a href="Draw_Me.html">Draw-Game</a>
+    <a href="Craps_Game.html">Craps-Game</a>
+    <a href="winestore.php">Wine Store</a>
+  </div>
+        </li>
+        </ul>
+        <ul class="dropdown">
+        <li><a href="Gallery.html">Gallery</a>
+ <div class="dropdown-content">
+    <a href="Draw_Me.html">Career</a>
+    <a href="Craps_Game.html">Future Events</a>
+    <a href="awards.html">Awards</a>
+  </div>
+		</li>
+        </ul><li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="home.html">Logout</a></li>
+        
+      </ul>
+    </nav>
+  </header>
+</div>
+<!-- content -->
+<div class="wrapper row2">
+ <p><section id="strings"></section></p>
+  <div id="container" class="clear">
+    
+    <!-- main content -->
+    <div id="homepage">
+    
+      <section id="intro" class="last clear">
+        <article>
+          <figure><h2>Welcome to Our Winestore</h2>
+              <p>Most people have had a pleasant experience with wine – over a nice dinner, at an event or simply with friends at home. Not many can recall the pleasure of shopping for wine.</p><p>Our goal is to provide a fun, educational and non-intimidating shopping experience for wine lovers of all levels of expertise</p>
+              <footer class="more"><a href="http://136.176.32.60/FashionClub/query.php"><h3>Explore our Wine Store &raquo;</h3></a></footer>
+             <br><br>
+            <figcaption>
+               </figcaption>
+          </figure>
+        </article>
+        <article class="one_third">
+         
+          <figure><img src="images/3wines.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Fashion Camp,Bradley University-2017 
+            
+              
+            </figcaption>
+          </figure>
+        </article>
+        <article class="one_third">
+          <figure><img src="images/wine-tasting.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Summer Fashion-Paris, 2017 
+              
+              
+            </figcaption>
+          </figure>
+        </article>
+        <article class="one_third lastbox">
+          <figure><img src="images/wine.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Fashion Camp- 2017
+              
+              
+            </figcaption>
+          </figure>
+        </article>
+        <article class="one_third">
+         
+          <figure><img src="images/Glasses-of-wine-002.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Fashion Camp,Bradley University-2017 
+            
+              
+            </figcaption>
+          </figure>
+        </article>
+        <article class="one_third">
+          <figure><img src="images/malvasia-winery.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Summer Fashion-Paris, 2017 
+              
+              
+            </figcaption>
+          </figure>
+        </article>
+        <article class="one_third lastbox">
+          <figure><img src="images/wine4.jpg" width="290" height="250" alt="">
+            <figcaption>
+              Fashion Camp- 2017
+              
+              
+            </figcaption>
+          </figure>
+        </article>
+      </section>
+      <!-- / Introduction -->
+    </div>
+    <!-- / content body -->
+  </div>
+</div>
+<!-- Footer -->
+<div class="wrapper row3">
+  <div id="footer" class="clear">
+  <div id="media">
+<h2 class="title">Connect with Us</h2>
+      <nav>
+        <ul>
+          <li><a href="https://www.facebook.com"><img src="images/icons/facebook.png" width="32" height="32" alt=""/></a></li>
+          <li><a href="https://www.instagram.com"><img src="images/icons/instagram.png" width="32" height="32" alt=""/></a></li>
+          <li><a href="https://www.twitter.com"><img src="images/icons/twitter.png" width="32" height="32" alt=""/></a></li>
+          <li><a href="https://www.googleplus.com"><img src="images/icons/google-plus.png" width="32" height="32" alt=""/></a></li>
+        </ul>
+      </nav>
+      </div>
+      <br><br><br><br>
+    <!-- Section One -->
+    <section class="one_quarter">
+      <h2 class="title">For Education</h2>
+      <nav>
+        <ul>
+          <li><a href="http://www.bradley.edu/">Fashion and Education</a></li>
+          <li><a href="http://www.bradley.edu/">Designing Tutorial</a></li>
+          <li><a href="http://www.bradley.edu/">Shop for College</a></li>
+          <li><a href="http://www.bradley.edu/">Accessories</a></li>
+        </ul>
+      </nav>
+    </section>
+    <!-- Section Two -->
+    <section class="one_quarter">
+      <h2 class="title">See Our Fashion Collection</h2>
+      <nav>
+        <ul>
+          <li><a href="https://www.amazon.com/"><img src="images/icons/amazon.png" width="32" height="16" alt=""/></a></li>
+          <li><a href="https://www.ebay.com/"><img src="images/icons/ebay.png" width="32" height="16" alt=""/></a></li>
+          <li><a href="https://www.foursquare.com/"><img src="images/icons/foursquare.png" width="32" height="16" alt=""/></a></li>
+          <li><a href="https://www.pinterest.com/"><img src="images/icons/pinterest.png" width="32" height="16" alt=""/></a></li>
+          
+        </ul>
+      </nav>
+    </section>
+    <!-- Section Three -->
+    <section class="one_quarter">
+      <h2 class="title">Know about Fashion</h2>
+      <nav>
+        <ul>
+          <li><a href="#"></a></li>
+          <li><a href="#">Newyork Fashion week</a></li>
+          <li><a href="#">Summer Fashion -Paris</a></li>
+          <li><a href="#">Women's Fashion Style</a></li>
+          <li><a href="#">Victoria Secret</a></li>
+        </ul>
+      </nav>
+    </section>
+    
+    <!-- Section Four -->
+    <section class="one_quarter lastbox">
+      <h2 class="title">Career</h2>
+      <nav>
+        <ul>
+          <li><a href="http://www.bradley.edu/campuslife/">Browse Jobs</a></li>
+          <li><a href="http://www.bradley.edu/admissions/">Our Employers</a></li>
+          <li><a href="http://www.bradley.edu/academic/colleges/cfa/">Internships</a></li>
+          <li><a href="#http://www.bradley.edu/scc/">Job Advice</a></li>
+        </ul>
+      </nav>
+    </section>
+    <!-- / Section -->
+    
+    
+  </div>
+</div>
+<!-- Copyright -->
+<div class="wrapper row4">
+  <footer id="copyright" class="clear">
+    <p class="fl_left">Copyright &copy; 2017 - <a href="#">Bradley University</a></p>
+    <p class="fl_right">By Mahananda Nyamagouda</p>
+  </footer>
+</div>
+</body>
+</html>
+
